e9352b454390b9bd8708efc0ec6ec56da42b12ae
Nikola Perisic <cathode.y2k@gmail.com>
2019-02-20 12:28:31 +0100

    minor styling tweaks, fixed links

--- src/assets/markup/markup.js
+++ src/assets/markup/markup.js
@@ -15,7 +15,7 @@ alert(s);
 
 Image with alt :
 
-![picture alt](http://www.brightlightpictures.com/assets/images/portfolio/thethaw_header.jpg "Title is optional")
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
 
 __Strong text__ or **Strong text**
 
