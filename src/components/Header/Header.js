694206860afcabba15c1ca88b8b24ed7bc63448a
Nikola Perisic <cathode.y2k@gmail.com>
2019-03-06 22:40:40 +0100

    fix vh mob browser

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,20 +3,20 @@ import logo from "../../logo.svg";
 import classes from "../../GlobalCSS//main.module.css";
 
 const header = props => {
-	return (
-		<header className={classes.header}>
-			<img src={logo} className={classes.logo} alt="logo" />
-			<p>{props.title}</p>
-			<a
-				className={classes.header_link}
-				href="https://github.github.com/gfm/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Github gfm spec
-			</a>
-		</header>
-	);
+    return (
+        <header className={classes.header}>
+            <img src={logo} className={classes.logo} alt="logo" />
+            <p>{props.title}</p>
+            <a
+                className={classes.header_link}
+                href="https://guides.github.com/features/mastering-markdown/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Github markdown spec
+            </a>
+        </header>
+    );
 };
 
 export default header;
